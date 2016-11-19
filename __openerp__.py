20dfa2edbe0202a368d0ef0b678939df2c25dcb2
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-19 11:47:34 -0300

    Removed dependency

--- __openerp__.py
+++ __openerp__.py
@@ -1,9 +1,9 @@
 {
-    'name': 'Informacion estadistica de productos',
+    'name': 'Informacion estadistica de productos v2.0',
     'category': 'Sale',
     'version': '0.1',
     #'depends': ['base','product','account','ba_sales'],
-    'depends': ['base','product','account','ba_sales'],
+    'depends': ['base','product','account'],
     'data': [
 	'product_view.xml',
 	'wizard/wizard_view.xml'
