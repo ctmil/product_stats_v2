8035974484bbe2445eb04cc1a4325287595707f6
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-19 12:23:09 -0300

    Added wizard

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -8,8 +8,8 @@ _logger = logging.getLogger(__name__)
 from openerp import models, fields, api
 
 
-class product_generate_abastecimiento(models.TransientModel):
-	_name= 'product.generate.abastecimiento'
+class product_generate_abastecimiento_v2(models.TransientModel):
+	_name= 'product.generate.abastecimiento.v2'
 
 	warehouse_id = fields.Many2one('stock.warehouse',string='Almacen',required=True)
 	#location_id = fields.Many2one('stock.location',string='Ubicacion',required=True,domain=[('usage','=','internal')])
@@ -22,15 +22,15 @@ class product_generate_abastecimiento(models.TransientModel):
 				product = self.env['product.product'].browse(active_id)
 				#lot_stock_id
 				#import pdb;pdb.set_trace()
-				if product.punto_pedido:
+				if product.punto_pedido_v2:
 					vals = {
 						'warehouse_id': self.warehouse_id.id,
 						'location_id': self.warehouse_id.lot_stock_id.id,
 						'product_id': active_id,
 						'active': True,
-						'product_min_qty': product.punto_pedido,
+						'product_min_qty': product.punto_pedido_v2,
 						'qty_multiple': 1,
-						'product_max_qty': product.punto_pedido + product.order_size,
+						'product_max_qty': product.punto_pedido_v2 + product.order_size_v2,
 						}
 					orderpoint_id = self.env['stock.warehouse.orderpoint'].search([('product_id','=',active_id),\
 									('warehouse_id','=',self.warehouse_id.id),\
