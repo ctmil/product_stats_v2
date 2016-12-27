56cbc41bd033df10f70462aed61a52ade0340ade
morrillo <gustavo.orrillo@moldeo.coop>
2016-12-27 20:20:04 -0300

    Fixed bug

--- models.py
+++ models.py
@@ -263,7 +263,8 @@ class product_product(models.Model):
 	@api.one
 	def _compute_semanas_stock_v2(self):
 		units_week = self.promedio_v2 / 4
-		self.semanas_stock_v2 = math.ceil(self.qty_available / units_week)
+		if units_week > 0:
+			self.semanas_stock_v2 = math.ceil(self.qty_available / units_week)
 
 	@api.one
 	def _compute_porc_vtas_a(self):
